a5b57fd2728d6220ac411c0ab225d6d056427a52
moxin <39158178+136155330@users.noreply.github.com>
2020-11-21 15:53:43 +0800

    Add files via upload

---
+++ linux_c++_5_7/nginx/app/dep/ngx_setproctitle.d
@@ -0,0 +1,3 @@
+/mnt/hgfs/linux/nginx/app/link_obj/ngx_setproctitle.o: ngx_setproctitle.cxx \
+ /mnt/hgfs/linux/nginx/_include/ngx_global.h \
+ /mnt/hgfs/linux/nginx/_include/ngx_c_socket.h
---
+++ linux_c++_5_7/nginx/app/dep/ngx_signal.d
@@ -0,0 +1,4 @@
+/mnt/hgfs/linux/nginx/app/link_obj/ngx_signal.o: ngx_signal.cxx /mnt/hgfs/linux/nginx/_include/ngx_global.h \
+ /mnt/hgfs/linux/nginx/_include/ngx_c_socket.h \
+ /mnt/hgfs/linux/nginx/_include/ngx_macro.h \
+ /mnt/hgfs/linux/nginx/_include/ngx_func.h
